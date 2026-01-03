70e67d2b34cd6b4bc3972a6f82b1aed61d7485db
Samuel <samuelgomes101112@gmail.com>
2026-01-03 11:03:38 -0300

    crm-1: instaling livewire 3

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Welcome::class);
 
 Route::get('testing', function () {
 
